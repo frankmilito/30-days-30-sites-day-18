0163b2f3633e0c4d887a9941189742a5ca62dfed
frankmilito <frankobidike20@gmail.com>
2020-09-20 16:40:53 -0500

    day 18 challenge

---
+++ app.js
@@ -0,0 +1,13 @@
+(function ($){
+    "use strict";
+    var mixer = mixitup('.portfolio-items',{
+        animation:{
+            effectsOut:'fade translateZ(-100%)'
+        }
+    })
+})(jQuery);
+
+// slick nav
+$('#menu').slicknav({
+    label:'MENU',
+})
